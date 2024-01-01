52c0da850b7fe212e6e15b354e82185007c061eb
LF <93954554+LeadsFarmer@users.noreply.github.com>
2024-01-01 20:41:17 +0100

    Update ShareChatbot.js

--- packages/ui/src/views/chatflows/ShareChatbot.js
+++ packages/ui/src/views/chatflows/ShareChatbot.js
@@ -401,7 +401,7 @@ const ShareChatbot = ({ isSessionMemory }) => {
                             onSwitchChange(event.target.checked)
                         }}
                     />
-                    <Typography>Make Public</Typography>
+                    <Typography>Rendre publique</Typography>
                     <TooltipWithParser
                         style={{ marginLeft: 10 }}
                         title={'Making public will allow anyone to access the chatbot without username & password'}
